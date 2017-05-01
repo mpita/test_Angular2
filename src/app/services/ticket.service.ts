d3d63184a54dead7079b29c2cf8843caabcb3b05
Manuel Pita <mpita1984@gmail.com>
2017-05-01 02:10:00 +0200

    add Route params observable

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { TICKETS } from './mocks/tickets.mock';
 
+import {Observable} from 'rxjs/Observable';
+
 @Injectable()
 export class TicketService {
 
@@ -18,4 +20,12 @@ export class TicketService {
         let ticket = TICKETS.find(x => x.id == id);
         return ticket;
     }
+
+    getTicketObserver(id) {
+        return Observable.create(observer => {
+            setTimeout(() => {
+            observer.next(TICKETS.find((ticket) => ticket.id == id))
+        }, 3000);
+    });
+  }
 }
